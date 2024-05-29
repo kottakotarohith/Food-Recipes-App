4f9f1e216cc0e2d9789a95a789ed9805c1fcdce0
Rohith Kottakota <rohithkottakota@gmail.com>
2024-05-29 23:30:24 +0530

    user registration and login done in backend

--- backend/APIs/user-api.js
+++ backend/APIs/user-api.js
@@ -1,18 +1,63 @@
 //creating mini express userApp
 const exp = require('express');
 const userApp = exp.Router();
+const bcryptjs = require('bcryptjs')
+const expressAsyncHandler = require('express-async-handler')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-userApp.get('/test-user' , (req,res)=>{
-    res.send({message: "this is from test user"})
+
+
+
+let usersCollection
+//get usersCollection object ( using middleware)
+userApp.use((req, res, next) => {
+    usersCollection = req.app.get('usersCollection')
+    next()
 })
 
 
+//user registration route
+userApp.post('/user', expressAsyncHandler(async (req, res) => {
+
+    //get new user data from client
+    const newUser = req.body;
+    //check if username is already existing or not
+    const dbUser = await usersCollection.findOne({ username: newUser.username })
+    if (dbUser != null) {
+        res.send({ message: "the username is already taken" })
+    } else {
+        //hash the password
+        const hashedPassword = await bcryptjs.hash(newUser.password, 7)
+        //replace password with hashed password
+        newUser.password = hashedPassword
+        //adding the user data to db
+        await usersCollection.insertOne(newUser)
+        //send res
+        res.send({ message: "new user created" })
+    }
+
+}))
 
 
-
-
-
-
+//user login route
+userApp.post('/login', expressAsyncHandler(async (req, res) => {
+    //user credentials
+    const userCredentials = req.body
+    //checking if user exists
+    const dbUser = await usersCollection.findOne({ username: userCredentials.username })
+    if (dbUser == null) {
+        res.send({ message: "invalid username" })
+    } else {
+        const passwordMatch = await bcryptjs.compare(userCredentials.password, dbUser.password)
+        if (!passwordMatch) {
+            res.send({ message: "invalid password" })
+        } else {
+            const signedToken = jwt.sign({ username: dbUser.username }, process.env.SECRET_KEY, { expiresIn: '1d' })
+            res.send({ message: "login successful", token: signedToken, user: dbUser })
+        }
+    }
+}))
 
 
 //export userApp
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,30 @@
 const exp = require("express")
 const app = exp()
 require('dotenv').config()
-
+const mongoClient = require('mongodb').MongoClient;
 
 //body parser (to parse the body req)
 app.use(exp.json())
+
+
+//connect to db
+mongoClient.connect(process.env.DataBase_URL)
+.then(client=>{
+    //get db object
+    const foodRecipeApp = client.db('foodRecipeApp')
+    //get collection obj
+    const usersCollection = foodRecipeApp.collection('usersCollection')
+    
+    //share collection obj with express app
+    app.set('usersCollection',usersCollection)
+    //confirm connection status
+    console.log("DataBase connection successful")
+
+})
+.catch(err=>console.log("Error in DataBase connection"))
+
+
+
 
 
 //importing api routes
@@ -18,8 +38,9 @@ app.use('/user-api' , userApp)
 
 //express error handler
 app.use((err,req,res,next)=>{
-    res.send({message : "error", payload:err})
+    res.send({message:"error occurred" , payload:err.message})
 })
+
 
 //assign port number
 const port = process.env.PORT || 2721
