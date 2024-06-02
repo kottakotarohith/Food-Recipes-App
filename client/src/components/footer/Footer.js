751c108256977aaa5f269b7aebf24ea8cf7616f3
Rohith Kottakota <rohithkottakota@gmail.com>
2024-06-03 00:16:52 +0530

    "Updated Footer component: changed CSS styles ."

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Footer.css'
+import logo from '../../imagesOfFoodRecipeApp/logo.png'
 
 function Footer() {
   return (
     <div>
         <div className='footer '>
-            <h3>Footer</h3>
+            <img src={logo}  alt='logo'  style={{height:"90px"}}/>
+            <p className='fs-5 text-dark-emphasis'>
+              Explore.Share.Cook.
+              <br/>
+              Dive into a community-curated collection of recipes.
+            </p>
+            
+            <p>© 2024 Food Recipe App. All Rights Reserved.</p>
         </div>
     </div>
   )
